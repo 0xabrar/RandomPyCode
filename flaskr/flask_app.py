4bf3326bb281dfeb2c5365872b58b0be399c0f57
Abrar Hussain <abrar.a.hussain@gmail.com>
2015-07-30 22:03:21 -0400

    small change

--- flaskr/flask_app.py
+++ flaskr/flask_app.py
@@ -1,80 +1,90 @@
 
-# A very simple Flask Hello World app for you to get started with...
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+from socket import gethostname
 
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-        abort, render_template, flash
 
-# configuration
-DATABASE = "/tmp/flaskr.db"
-DEBUG = True
-SECRET_KEY = "development key"
-USERNAME = "admin"
-PASSWORD = "default"
-
+# create our little application :)
 app = Flask(__name__)
-app.config.from_envar("FLASKR SETTINGS", silent=True)
 
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASK_APP_SETTINGS', silent=True)
 
 def connect_db():
-    return sqlite.connect(app.config["DATABASE"])
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 
 def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource("schema.sql", mode="r") as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
 
-@app.before_request
-def before_request():
-	g.db=connect_db()
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
-@app.teardown_request
-def teardown_request(exception):
-	db=getattr(g, "db", None)
-	if db is not None:
-		db.close()
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route('/')
 def show_entries():
-	cur=g.db.execute("select title, text from entries order by id desc")
-	entries=[dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-	return render_template("show_entries.html", entries=entries)
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
 
 
-@app.route('/add', methods=["POST"])
+@app.route('/add', methods=['POST'])
 def add_entry():
-	if not session.get("logged_in"):
-		abort(401)
-	g.db.execute('insert into entries (title, text) values (?, ?)',
-			[request.form['title'], request.form['text']])
-	g.db.commit()
-	flash("New entry was successfully posted.")
-	return redirect(url_for("show_entries"))
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+               [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
 
-@app.route("/login", methods=["GET", "POST"])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	error=None
-	if request.form['username'] != app.config['USERNAME']:
-		error="Invalid username"
-	elif request.form["password"] != app.config["PASSWORD"]:
-		error="Invalid password"
-	else:
-		session["logged_in"]=True
-		flash("You were logged in.")
-		return redirect(url_for("show_entries"))
-	return render_template("login.html", error=error)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
 
-@app.route("/logout")
+
+@app.route('/logout')
 def logout():
-	session.pop("logged_in", None)
-	flash("You were logged out.")
-	return redirect(url_for("show_entries"))
-
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-    
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
