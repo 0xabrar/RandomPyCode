151abdc6f74339afe5cfd23c19d113f29fc78a46
Abrar Hussain <abrar.a.hussain@gmail.com>
2015-07-31 22:28:30 -0400

    added alternative rectangle to paint

--- kivy/paint.py
+++ kivy/paint.py
@@ -1,36 +1,46 @@
-from kivy import random
+from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.graphics import Color, Eclipse, Line
+from kivy.graphics import Color, Ellipse, Line, Rectangle
 
 
 class PaintWidget(Widget):
+
+    shaped_Rectangle = True
 
     def on_touch_down(self, touch):
         color = (random(), 1, 1)
         with self.canvas:
             Color(*color, mode='hsv')
             d = 30.
-            Eclipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            if PaintWidget.shaped_Rectangle:
+            	Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            	PaintWidget.shaped_Rectangle = False
+            else:
+            	Rectangle(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+            	PaintWidget.shaped_Rectangle = True
+            
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
 
     def on_touch_move(self, touch):
-    	touch.ud(['line'].points += [touch.x, touch.y]
+        touch.ud['line'].points += [touch.x, touch.y]
 
 
-class PaintApp(Widget):
+class PaintApp(App):
 
-	def build(self):
-		parent = Widget()
-		self.painter = MyPaintWidget()
-		clear_button = Button(text='clear')
-		clear_button.bind(on_release=self.clear_canvas)
-		parent.add_widget(self.painter)
-		parent.add_widget(clear_button)
-		return parent
+    def build(self):
+        parent = Widget()
+        self.painter = PaintWidget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
 
-	def clear_canvas(self, obj):
-		self.painter.canvas.clear()
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
-if __name__ == "__main__":
-	PaintApp.run()
+
+if __name__ == '__main__':
+    PaintApp().run()
