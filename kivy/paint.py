3ef2005b6e88f25c88187c6286b0675faabaa6a4
Abrar Hussain <abrar.a.hussain@gmail.com>
2015-08-02 02:05:12 -0400

    added in small menu

--- kivy/paint.py
+++ kivy/paint.py
@@ -3,12 +3,15 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line, Rectangle, Triangle
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
 
 
 class PaintWidget(Widget):
 
-    shaped_Rectangle = 1
-
+    def __init__(self, **kwargs):
+        Widget.__init__(self, **kwargs)
+        self.shaped_Rectangle = 1
 
     # shuffle between: circle, triangle, rectangle
     def on_touch_down(self, touch):
@@ -16,17 +19,17 @@ class PaintWidget(Widget):
         with self.canvas:
             Color(*color, mode='hsv')
             d = 30.
-            if PaintWidget.shaped_Rectangle == 1:
+            if self.shaped_Rectangle == 1:
                 Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-                PaintWidget.shaped_Rectangle = 2
-            elif PaintWidget.shaped_Rectangle == 2:
+                self.shaped_Rectangle = 2
+            elif self.shaped_Rectangle == 2:
                 coords = [touch.x - d / 2, touch.y - d / 2, touch.x,
-                        touch.y + d / 2, touch.x + d / 2, touch.y - d / 2]
+                          touch.y + d / 2, touch.x + d / 2, touch.y - d / 2]
                 Triangle(points=coords)
-                PaintWidget.shaped_Rectangle = 3
+                self.shaped_Rectangle = 3
             else:
                 Rectangle(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-                PaintWidget.shaped_Rectangle = 1
+                self.shaped_Rectangle = 1
 
             touch.ud['line'] = Line(points=(touch.x, touch.y))
 
@@ -34,19 +37,18 @@ class PaintWidget(Widget):
         touch.ud['line'].points += [touch.x, touch.y]
 
 
+class Menu(FloatLayout):
+
+    paint_canvas = ObjectProperty(None)
+
+    def clear_canvas(self):
+        self.paint_canvas.canvas.clear()
+
+
 class PaintApp(App):
 
     def build(self):
-        parent = Widget()
-        self.painter = PaintWidget()
-        clearbtn = Button(text='Clear')
-        clearbtn.bind(on_release=self.clear_canvas)
-        parent.add_widget(self.painter)
-        parent.add_widget(clearbtn)
-        return parent
-
-    def clear_canvas(self, obj):
-        self.painter.canvas.clear()
+        return Menu()
 
 
 if __name__ == '__main__':
