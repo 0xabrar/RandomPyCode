93276ff1ef7defcc94a37894d65375bb77797981
Abrar Hussain <abrar.a.hussain@gmail.com>
2015-08-05 23:39:32 -0400

    fixed orientation and set top time

--- kivy/main.py
+++ kivy/main.py
@@ -5,39 +5,38 @@ from kivy.utils import get_color_from_hex
 from kivy.clock import Clock
 from time import strftime
 
+
 class ClockApp(App):
 
+    sw_started = False
+    sw_seconds = 0
 
-	sw_started = False
-	sw_seconds = 0
+    def start_stop(self):
+        self.root.ids.start_stop.text = ("Start"
+                                         if self.sw_started else "Stop")
+        self.sw_started = not self.sw_started
 
+    def reset(self):
+        self.sw_seconds = 0
 
+    # time is updated using PYthon (not Kivy) interface
+    def update_time(self, nap):
+        if self.sw_started:
+            self.sw_seconds += nap
 
-	def  start_stop(self):
-		self.root.ids.start_stop.text = ("Start" 
-			if self.sw_started else "Stop")
-		self.sw_started = not self.sw_started
+        minutes, seconds = divmod(self.sw_seconds, 60)
+        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
 
+        self.root.ids.stopwatch.text = (
+            "%02d:%02d. [size=40]%02d[/size]" %
+            (int(minutes), int(seconds), int(seconds * 100 % 100)))
 
-	def reset(self):
-		self.sw_seconds = 0
-
-	# time is updated using PYthon (not Kivy) interface
-	def update_time(self, nap):
-		if self.sw_started:
-			self.sw_seconds += nap
-			minutes, seconds = divmod(self.sw_seconds, 60)
-			self.root.ids.stopwatch.text = (
-				"%02d:%02d. [size=40]%02d[/size]" %
-					(int(minutes), int(seconds), int(seconds * 100 % 100)))
-
-
-	def on_start(self):
-		Clock.schedule_interval(self.update_time, 0)
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 0)
 
 if __name__ == "__main__":
-	Window.clearcolor = get_color_from_hex("#101216")
-	LabelBase.register(name = "Roboto",
-		fn_regular = "Roboto-Thin.ttf",
-		fn_bold = "Roboto-Medium.ttf")
-	ClockApp().run() 
+    Window.clearcolor = get_color_from_hex("#101216")
+    LabelBase.register(name="Roboto",
+                       fn_regular="Roboto-Thin.ttf",
+                       fn_bold="Roboto-Medium.ttf")
+    ClockApp().run()
